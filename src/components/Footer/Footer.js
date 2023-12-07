98b1e33e614d6b8b4995cf16de0ec17c7ed92f91
1nchxn <143166866+1nchxn@users.noreply.github.com>
2023-12-08 04:38:20 +0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <h2>Created in 2023 by Christian B. Saron & Mark Jay Neri || Final Project for CC106 || BSCS-3C</h2>
+      <h2>Created by Christian B. Saron & Mark Jay Neri || Final Project for CC106 || BSCS-3C</h2>
     </div>
   );
 };
